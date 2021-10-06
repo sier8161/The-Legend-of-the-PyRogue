db5551cea4c86eacf29ad5b62c57b1ddab0a62eb
sier8161 <simon.eriksson8161@student.uu.se>
2021-10-06 12:23:02 +0200

    Update main.py
    basic rumsgenerering nu fullt fungerande, dock med den dåliga algoritmen

--- main.py
+++ main.py
@@ -1,44 +1,82 @@
-ROOMSIZE = (21,21) # (x,y)
+ROOMSIZE = 21 # (x,y)
 
-H_WALL = "═" # Basic grafik inläst som konstanter
+H_WALL = "══" # Basic tile-grafik inläst som konstanter
 V_WALL = "║"
 TL_CORNER = "╔"
 TR_CORNER = "╗"
 BL_CORNER = "╚"
 BR_CORNER = "╝"
+HALFEMPTY = " "
+EMPTY = "  "
 
-T_DOOR = BR_CORNER+" "+BL_CORNER
-B_DOOR = TR_CORNER+" "+TL_CORNER
-L_DOOR = BR_CORNER+"\n"+TR_CORNER
-R_DOOR = BL_CORNER+"\n"+TL_CORNER
-
+T_DOOR  = BR_CORNER+2*EMPTY+BL_CORNER
+B_DOOR  = TR_CORNER+2*EMPTY+TL_CORNER
 TB_NODOOR = H_WALL*3
-LR_NODOOR = V_WALL*3
 
 def generate_room(doors):
     room = []
-    for vertLine in range(ROOMSIZE[1]):
+    for vertLine in range(ROOMSIZE):
         horizLine = []
         if vertLine == 0:
-            horizLine.append(TL_CORNER)
-            horizLine.append(H_WALL*int(((ROOMSIZE[0]-3)/2)-1))
+            horizLine+=TL_CORNER
+            horizLine+=H_WALL*int(((ROOMSIZE-1)/2)-2)
             if doors['T']:
-                horizLine.append(T_DOOR)
+                horizLine+=T_DOOR
             else:
-                horizLine.append(TB_NODOOR)
-            horizLine.append(H_WALL*int(((ROOMSIZE[0]-3)/2)-1))
-            horizLine.append(TR_CORNER)
-        if vertLine == ROOMSIZE[1]-1:
-            horizLine.append(BL_CORNER)
-            horizLine.append(H_WALL*int(((ROOMSIZE[0]-3)/2)-1))
+                horizLine+=TB_NODOOR
+            horizLine+=H_WALL*int(((ROOMSIZE-1)/2)-2)
+            horizLine+=TR_CORNER
+        elif vertLine == ROOMSIZE-1:
+            horizLine+=BL_CORNER
+            horizLine+=H_WALL*int(((ROOMSIZE-1)/2)-2)
             if doors['B']:
-                horizLine.append(B_DOOR)
+                horizLine+=B_DOOR
             else:
-                horizLine.append(TB_NODOOR)
-            horizLine.append(H_WALL*int(((ROOMSIZE[0]-3)/2)-1))
-            horizLine.append(BR_CORNER)
+                horizLine+=TB_NODOOR
+            horizLine+=H_WALL*int(((ROOMSIZE-1)/2)-2)
+            horizLine+=BR_CORNER
+        elif vertLine == int(((ROOMSIZE-1)/2)-2):
+            if doors['L']:
+                horizLine+=BR_CORNER
+            else:
+                horizLine+=V_WALL
+            horizLine+=EMPTY*(ROOMSIZE-2)
+            if doors['R']:
+                horizLine+=BL_CORNER
+            else:
+                horizLine+=V_WALL
+        elif vertLine == int(((ROOMSIZE-1)/2)-1):
+            if doors['L']:
+                horizLine+=HALFEMPTY
+            else:
+                horizLine+=V_WALL
+            horizLine+=EMPTY*(ROOMSIZE-2)
+            if doors['R']:
+                horizLine+=HALFEMPTY
+            else:
+                horizLine+=V_WALL
+        elif vertLine == int(((ROOMSIZE-1)/2)):
+            if doors['L']:
+                horizLine+=TR_CORNER
+            else:
+                horizLine+=V_WALL
+            horizLine+=EMPTY*(ROOMSIZE-2)
+            if doors['R']:
+                horizLine+=TL_CORNER
+            else:
+                horizLine+=V_WALL
+        else:
+            horizLine+=V_WALL
+            horizLine+=EMPTY*(ROOMSIZE-2)
+            horizLine+=V_WALL
         room.append(horizLine)
     return room
-doors = {"T":True,"L":True,"R":False,"B":False}
 
-test = generate_room(doors)
+doors = {"T":False,"L":True,"R":False,"B":True}
+
+test_room = generate_room(doors)
+for hLine in test_room:
+    currLine = ""
+    for tile in hLine:
+        currLine += tile
+    print(f"{currLine}")
