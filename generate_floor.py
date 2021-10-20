fac0996fe75b0c4d377f6f9908fe96777f57df27
vfe01 <villeelings@gmail.com>
2021-10-20 18:33:58 +0200

    Gjorde egen pathfinding
    Försökte avlusa a* men det gick inte bra. Eftersom entiteter inte kan röra sig diagonalt eller flera steg åt gången lade jag till en simpel pathfindingalgoritm som gör jobbet.

--- generate_floor.py
+++ generate_floor.py
@@ -1,4 +1,5 @@
 from random import randint
+from random import shuffle
 import keyboard
 import os
 from time import sleep
@@ -367,108 +368,36 @@ def enemy_turn():
     currentRoom = floor[entities['PLAYER']['room']]
     for e in list(entities)[1:]:
         if entities[e]['room'] == entities['PLAYER']['room'] and entities[e]['life'] != 0:
-            path = astar(currentRoom['tiles'], e, entities['PLAYER']['pos'])
-            entity_action(e, path[1])
+            entity_action(e, pathfinder(currentRoom['tiles'], e, entities['PLAYER']['pos']))
             
 
-#A* pathfinding
+#egengjord pathfinder
 
-class Node():
-    def __init__(self, parent=None, position=None):
-        self.parent = parent
-        self.position = position
-
-        self.g = 0
-        self.h = 0
-        self.f = 0
-
-    def __eq__(self, other):
-        return self.position == other.position
-
-
-def astar(maze, entity, where):
-
-    # Create start and end node
-    start_node = Node(None, entities[entity]['pos'])
-    start_node.g = start_node.h = start_node.f = 0
-    end_node = Node(None, where)
-    end_node.g = end_node.h = end_node.f = 0
-
-    # Initialize both open and closed list
-    open_list = []
-    closed_list = []
-
-    # Add the start node
-    open_list.append(start_node)
-
-    # Loop until you find the end
-    while len(open_list) > 0:
-
-        # Get the current node
-        current_node = open_list[0]
-        current_index = 0
-        for index, item in enumerate(open_list):
-            if item.f < current_node.f:
-                current_node = item
-                current_index = index
-
-        # Pop current off open list, add to closed list
-        open_list.pop(current_index)
-        closed_list.append(current_node)
-
-        # Found the goal
-        if current_node == end_node:
-            path = []
-            current = current_node
-            while current is not None:
-                path.append(current.position)
-                current = current.parent
-            return path[::-1] # Return reversed path
-
-        # Generate children
-        children = []
-        for new_position in [(0, -1), (0, 1), (-1, 0), (1, 0), (-1, -1), (-1, 1), (1, -1), (1, 1)]: #Skulle vara optimalt att göra så att monsterns path inte kan vara diagonalt men spelet softlockar om monstret är på en diagonal ruta intill spelaren
-
-            # Get node position
-            node_position = (current_node.position[0] + new_position[0], current_node.position[1] + new_position[1])
-
-            # Make sure within range
-            if node_position[0] > (SIDELENGTH) or node_position[0] < 0 or node_position[1] > (SIDELENGTH) or node_position[1] < 0:
-                continue
-
-            # Make sure walkable terrain
-            if maze[node_position[0]][node_position[1]] != GRAPHICS['EMPTY']:
-                continue
-
-            # Create new node
-            new_node = Node(current_node, node_position)
-
-            # Append
-            children.append(new_node)
-
-        # Loop through children
-        for child in children:
-
-            # Child is on the closed list
-            for closed_child in closed_list:
-                if child == closed_child:
-                    continue
-
-            # Create the f, g, and h values
-            child.g = current_node.g + 1
-            child.h = ((child.position[0] - end_node.position[0]) ** 2) + ((child.position[1] - end_node.position[1]) ** 2)
-            child.f = child.g + child.h
-
-            # Child is already in the open list
-            for open_node in open_list:
-                if child == open_node and child.g > open_node.g:
-                    continue
-
-            # Add the child to the open list
-            open_list.append(child)
-
-      
-
+#A* är overkill och kan inte avlusa det och eftersom entiteter inte kan röra sig diagonalt och fler steg i taget så är det enkelt att programmera en egen
+def pathfinder(tiles, entity, target):
+    eX, eY = entities[entity]['pos']
+    targetX, targetY = target
+    direction = {'up': (eX, eY-1),
+               'left': (eX-1, eY),
+               'right': (eX+1, eY),
+               'down': (eX, eY+1)}
+    directions = []
+    if targetX < eX:
+        directions.append('left')
+    if targetX > eX:
+        directions.append('right')
+    if targetY < eY:
+        directions.append('up')
+    if targetY > eY:
+        directions.append('down')
+        
+    shuffle(directions) #Gör detta så att movement ser bättre ut
+    for possible_direction in directions:
+        pos = direction[possible_direction]
+        if pos == target:
+            return pos
+        elif tiles[pos[1]][pos[0]] == GRAPHICS['EMPTY']:
+            return pos
     
 #Testfunktioner
 def testing_create_doors(door):
