f16037a71eebf3109c6e775ec162189db809a259
sier8161 <simon.eriksson8161@student.uu.se>
2021-10-23 19:09:28 +0200

    la till en kommentar

--- generate_floor.py
+++ generate_floor.py
@@ -49,7 +49,7 @@ def initGlobalVariables():
     entities = {'PLAYER':{'pos':(MIDDLE, MIDDLE),
                     'room':0,  #OBS! Nyckel 'room' som en entitet har är ett index för listan floor där indexet motsvarar ett dictionary som är rummet i fråga
                     'life':2, #2: sköld, 1: ingen sköld, 0:död
-                    'evasion': 10,
+                    'evasion': 20, #sätts efter mainMenu till (4-difficulty)*10 (easy = 30, normal = 20, hard = 10)
                     'name':'Player'
                     }
                 #MONSTER_1 , MONSTER_2, osv till MONSTER_{monstersAlive} kommer finnas i denna lista efter att de genererats
